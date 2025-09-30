da42e17c02461843f0363aceff17c4ba9cf53192
Cloudy <cloudisms@protonmail.com>
2025-09-30 19:16:13 -0400

    Rename to Stellar

--- gif.sh
+++ gif.sh
@@ -1,2 +1,2 @@
-ffmpeg -i images/CGPA1%02d.png -vf "palettegen" images/palette.png
-ffmpeg -framerate 24 -i images/CGPA1%02d.png -i images/palette.png  -filter_complex "paletteuse" output.gif
+ffmpeg -i images/Stellar%02d.png -vf "palettegen" images/palette.png
+ffmpeg -framerate 24 -i images/Stellar%02d.png -i images/palette.png  -filter_complex "paletteuse" Stellar.gif
