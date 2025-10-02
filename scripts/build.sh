ff78fc0a316072a2144a2e84182dd51eeea2af1e
Cloudy <cloudisms@protonmail.com>
2025-10-02 03:02:16 -0400

    Configure build script

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+set -e
+
+# This script provides a consistent way to build the GIF locally
+# using the same Docker environment as the GitHub Actions workflow.
+
+IMAGE_NAME="stellar"
+
+echo "--- Building Docker image: $IMAGE_NAME ---"
+docker build -t "$IMAGE_NAME" .devcontainer
+
+echo "--- Running gif generation script inside Docker ---"
+docker run --rm -v "$PWD:/source" "$IMAGE_NAME" scripts/gif.sh
