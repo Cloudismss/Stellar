006520e9fada2d7168882ab776a55229677abc96
Cloudy <cloudisms@protonmail.com>
2025-10-03 16:45:33 -0400

    Add shell argument

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,4 +9,4 @@ echo "--- Building Docker image: $IMAGE_NAME ---"
 docker build -t "$IMAGE_NAME" .devcontainer
 
 echo "--- Running gif generation script inside Docker ---"
-docker run --rm -v "$PWD:/source" "$IMAGE_NAME" scripts/gif.sh
+docker run --rm -v "$PWD:/source" "$IMAGE_NAME" /bin/sh scripts/gif.sh
